56514475e59c8f70823f98a685e52c7ccb7b13c2
muhammad-usman-108 <umuhammad202@yahoo.com>
2022-10-24 17:19:21 +0200

    Java Spring: Loose Coupling & Interfaces 2.0

--- learn-spring-framework/src/main/java/com/practice/spring/learnspringframework/LearnSpringFrameworkApplication.java
+++ learn-spring-framework/src/main/java/com/practice/spring/learnspringframework/LearnSpringFrameworkApplication.java
@@ -6,14 +6,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.practice.spring.learnspringframework.game.GameRunner;
 import com.practice.spring.learnspringframework.game.MarioGame;
 import com.practice.spring.learnspringframework.game.SuperContraGame;
+import com.practice.spring.learnspringframework.game.PacmanGame;
 
 @SpringBootApplication
 public class LearnSpringFrameworkApplication {
 
 	public static void main(String[] args) {
 		//SpringApplication.run(LearnSpringFrameworkApplication.class, args);
-		MarioGame game = new MarioGame();
+		//MarioGame game = new MarioGame();
 		//SuperContraGame game = new SuperContraGame();
+		PacmanGame game = new PacmanGame();
 		GameRunner runner = new GameRunner(game);
 		runner.run();
 	}
