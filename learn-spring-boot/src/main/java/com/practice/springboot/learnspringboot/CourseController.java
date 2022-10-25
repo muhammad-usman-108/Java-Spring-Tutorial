2bacced09f3ae6e7c4d92de6e367035bc8bc1a37
muhammad-usman-108 <umuhammad202@yahoo.com>
2022-10-25 16:34:33 +0200

    Java Spring Boot: Initial application

---
+++ learn-spring-boot/src/main/java/com/practice/springboot/learnspringboot/CourseController.java
@@ -0,0 +1,19 @@
+package com.practice.springboot.learnspringboot;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CourseController {
+	
+	@RequestMapping("/courses")
+	public List<Course> retrieveAllCourse() {
+		return Arrays.asList(
+				new Course(1, "Learn AWS", "Udemy"),
+				new Course(2, "Learn DevOps", "Coursera")
+				);
+	}
+}
