b64e4f12d753c480be59912dc97f53b15ed003ef
muhammad-usman-108 <umuhammad202@yahoo.com>
2022-10-26 14:25:40 +0200

    Java Spring: Logging

--- myfirstwebapp/src/main/java/com/practice/springboot/myfirstwebapp/login/LoginController.java
+++ myfirstwebapp/src/main/java/com/practice/springboot/myfirstwebapp/login/LoginController.java
@@ -1,5 +1,7 @@
 package com.practice.springboot.myfirstwebapp.login;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +10,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class LoginController {
 	
+	private Logger logger = LoggerFactory.getLogger(getClass());
 	@RequestMapping("login")
 	public String gotoLogin(@RequestParam String name, ModelMap model) {
 		model.put("name", name);
+		logger.debug("Request param is {}", name);
+		logger.info("Request param is {}", name);
+		logger.warn("Request param is {}", name);
 		System.out.println("Request param is " + name);
 		return "login";
 	}
