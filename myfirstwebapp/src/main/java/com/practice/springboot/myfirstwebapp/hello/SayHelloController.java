97df1958b8289da4cd830afae80c308771c32d90
muhammad-usman-108 <umuhammad202@yahoo.com>
2022-10-26 12:09:00 +0200

    Java Spring: Web App, @Controller, @ResponseBody & HTML Response

---
+++ myfirstwebapp/src/main/java/com/practice/springboot/myfirstwebapp/hello/SayHelloController.java
@@ -0,0 +1,31 @@
+package com.practice.springboot.myfirstwebapp.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SayHelloController {
+	
+	@RequestMapping("say-hello")
+	@ResponseBody
+	public String sayHello() {
+		return "Hello! What are you learning today?";
+	}
+	
+	@RequestMapping("say-hello-html")
+	@ResponseBody
+	public String sayHelloHtml() {
+		StringBuffer sb = new StringBuffer();
+		sb.append("<html>");
+		sb.append("<head>");
+		sb.append("<title>My first HTML page</title>");
+		sb.append("</head>");
+		sb.append("<body>");
+		sb.append("My first html page with body");
+		sb.append("</body>");
+		sb.append("</html>");
+		return sb.toString();
+	}
+
+}
