55b8e5a51565aeb605b911c45c123b9b1c60dd8d
muhammad-usman-108 <umuhammad202@yahoo.com>
2022-10-30 14:48:31 +0100

    Java: Mockito Unit Test-1

---
+++ mockito-demo/src/main/java/com/practice/mockito/mockitodemo/business/SomeBusinessImpl.java
@@ -0,0 +1,25 @@
+package com.practice.mockito.mockitodemo.business;
+
+public class SomeBusinessImpl {
+	
+	private DataService dataService;
+	
+	SomeBusinessImpl(DataService dataService) {
+		super();
+		this.dataService = dataService;
+	}
+	
+	public int findTheGreatestFromAllData() {
+		int[] data = dataService.retrieveAllData();
+		int greatestValue = Integer.MIN_VALUE;
+		for(int value:data) {
+			if (value > greatestValue)
+				greatestValue = value;
+		}
+		return greatestValue;
+	}
+}
+
+interface DataService {
+	int[] retrieveAllData();
+}
---
+++ mockito-demo/src/test/java/com/practice/mockito/mockitodemo/business/SomeBusinessImplTest.java
@@ -0,0 +1,23 @@
+package com.practice.mockito.mockitodemo.business;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class SomeBusinessImplTest {
+	
+	@Test
+	void test() {
+		DataServiceStub dataServiceStub = new DataServiceStub();
+		SomeBusinessImpl businessImpl = new SomeBusinessImpl(dataServiceStub);
+		int result = businessImpl.findTheGreatestFromAllData();
+		assertEquals(25, result);
+	}
+}
+
+class DataServiceStub implements DataService {
+	@Override
+	public int[] retrieveAllData() {
+		return new int[] {25, 15, 5};
+	}
+}
